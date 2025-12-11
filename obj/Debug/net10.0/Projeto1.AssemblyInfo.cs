e6d416de99997cd91191604bee6b62a711c894a0
Vinicius Avila <viniciusvaln10@hotmail.com>
2025-12-11 02:32:24 -0300

    Aula 2 - Razor Pages

--- obj/Debug/net10.0/Projeto1.AssemblyInfo.cs
+++ obj/Debug/net10.0/Projeto1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1369b260a00d3226d3c5832308c7fc1c791edb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
