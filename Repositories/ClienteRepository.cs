d57818875c32f377ff195cd5797b2aa7c16800b1
Vinicius Avila <viniciusvaln10@hotmail.com>
2025-12-12 02:15:47 -0300

    Aula 7 - Interfaces e Repositorios

---
+++ Repositories/ClienteRepository.cs
@@ -0,0 +1,40 @@
+using Projeto1.Models;
+using Projeto1.Interfaces;
+namespace Projeto1.Repositories
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private readonly AppDbContext _context;
+        public ClienteRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Cliente>> ListarTodosAsync()
+        {
+            return await _context.Clientes.ToListAsync();
+        }
+        public async Task<Cliente?> ObterPorIdAsync(int id)
+        {
+            return await _context.Clientes.FindAsync(id);
+        }
+        public async Task IncluirAsync(Cliente cliente)
+        {
+            _context.Clientes.Add(cliente);
+            await _context.SaveChangesAsync();
+        }
+        public async Task AtualizarAsync(Cliente cliente)
+        {
+            _context.Clientes.Update(cliente);
+            await _context.SaveChangesAsync();
+        }
+        public async Task ExcluirAsync(int id)
+        {
+            var cliente = await ObterPorIdAsync(id);
+            if (cliente != null)
+            {
+                _context.Clientes.Remove(cliente);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
